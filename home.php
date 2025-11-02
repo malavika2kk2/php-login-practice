dfc7021cde91b10bc0c40e77922564c275e4ede6
malavika2kk2 <124487373+malavika2kk2@users.noreply.github.com>
2025-11-02 12:50:05 +0530

    Create home.php for user session management

---
+++ home.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+if(isset($_SESSION['id'])&& isset($_SESSION['user_name'])){
+    ?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>HOME</title>
+     <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+     <h1>Hello, <?php echo $_SESSION['user_name']; ?> </h1>
+     <a href="logout.php">Logout</a>
+    </body>
+    </html> 
+    <?php
+}
+else {
+    header("Location:index.php");
+    exit();
+}
+
+?>
+  
